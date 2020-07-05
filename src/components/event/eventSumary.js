267b9f1e916dde2a68aa7e3ac84be49a4a032204
Nguyễn Tấn Trung <46466731+trungdeveloper@users.noreply.github.com>
2020-07-05 15:27:29 +0700

    Huy-css (#6)
    Co-authored-by: trtnguyen <trung.tan.nguyen@mgm-tp.com>

--- src/components/event/eventSumary.js
+++ src/components/event/eventSumary.js
@@ -2,20 +2,17 @@ import React from "react";
 
 export const EventSummary = ({ event }) => {
     return (
-        <div>
-            <div>
-                <img
-                    alt="event"
-                    style={{ width: 100, height: 100 }}
-                    src={event.img}
-                />
+        <div className="event-item">
+            <div className="event-image"> 
+                <img alt="event" src={event.img}/>
             </div>
-            <div>
+            <div className="event-content">
                 <div>{event.title}</div>
                 <div>Vật phẩm trao tặng: {event.product}</div>
                 <div>Nhà tài trợ: {event.sponsor}</div>
                 <div>Ngày trao tặng: {event.date}</div>
                 <div>Địa điểm trao tặng:{event.location}</div>
+                <button className="event-btn-view-more">Xem thêm thông tin</button>
             </div>
         </div>
     );
--- src/components/plight/newPlight.js
+++ src/components/plight/newPlight.js
@@ -2,22 +2,15 @@ import React from "react";
 
 export const NewPlight = ({ plight }) => {
     return (
-        <div>
-            <div>
-                <img
-                    alt="plight"
-                    style={{ width: 100, height: 100 }}
-                    src={plight.img}
-                />
+        <div className="disadvantaged-item">
+            <div className="disadvantaged-image">
+                <img alt="plight" src={plight.img}/>
             </div>
-            <div>
-                <div>
-                    `${plight.name} - ${plight.need}`
-                </div>
+            <div className="disadvantaged-content">
+                <div>`${plight.name} - ${plight.need}`</div>
                 <div>{plight.description}</div>
-                <button>Tài Trợ Ngay</button>
+                <button className="disadvantaged-btn-donate">Tài Trợ Ngay</button>
             </div>
-            s
         </div>
     );
 };
