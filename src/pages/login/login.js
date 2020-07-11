b0c39ac8e7dfb60053dce308c8c22dbe7ed84830
trtnguyen <trung.tan.nguyen@mgm-tp.com>
2020-07-12 01:18:38 +0700

    login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,34 +1,62 @@
 import React from "react";
-export const Login = () => {
+import { connect } from "react-redux";
+import { loginRequest } from "../../redux/authentication/actions";
+
+const Login = ({ loginHandle }) => {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
     return (
         <div className="row">
-            <form action="#">
-                <div className="row">
-                    <h1>ĐĂNG NHẬP</h1>
+            <div className="row">
+                <h1>ĐĂNG NHẬP</h1>
+            </div>
+            <div className="row">
+                <label htmlFor="email" className="col">
+                    Email
+                </label>
+                <div className="col">
+                    <input
+                        id="email"
+                        type="email"
+                        value={email}
+                        onChange={(val) => setEmail(val.target.value)}
+                        placeholder="Email"
+                    />
                 </div>
-                <div className="row">
-                    <div className="col">Tên Đăng nhập</div>
-                    <div className="col">
-                        <input type="text" name="txtUsername" placeholder="Tên Đăng nhập" />
-                    </div>
+            </div>
+            <div className="row">
+                <label htmlFor="password" className="col">
+                    Mật Khẩu
+                </label>
+                <div className="col">
+                    <input
+                        id="password"
+                        type="password"
+                        value={password}
+                        onChange={(val) => setPassword(val.target.value)}
+                        placeholder="Mật KhẩU"
+                    />
                 </div>
-                <div className="row">
-                    <div className="col">Mật Khẩu</div>
-                    <div className="col">
-                        <input type="text" name="txtPass" placeholder="Mật KhẩU" />
-                    </div>
-                </div>
-                <div className="row">
-                    <input type="submit" value="ĐĂNG NHẬP BAWGF EMAIL" />
-                </div>
-                <div className="row">
-                    <input type="submit" value="ĐĂNG NHẬP BAWGF FACEBOOK" />
-                </div>
-                <div className="row">
-                    <input type="submit" value="ĐĂNG NHẬP" />
-                    <input type="submit" value="THOÁT" />
-                </div>
-            </form>
+            </div>
+            <button onClick={() => loginHandle({ email, password })}>
+                Đăng nhập
+            </button>
         </div>
     );
-}
+};
+
+const mapStateToProps = (state) => {
+    return {
+        data: state.user,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        loginHandle: (data) => {
+            dispatch(loginRequest(data));
+        },
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import eventReducer from "./event/reducer";
 import { combineReducers } from "redux";
+import userReducer from "./authentication/reducer";
 
 const rootReducer = combineReducers({
     event: eventReducer,
+    user: userReducer,
 });
 
 export default rootReducer;
