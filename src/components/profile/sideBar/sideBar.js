436df71a72037aeb549e487ddba3277b8a5eedba
trtnguyen <trung.tan.nguyen@mgm-tp.com>
2020-07-12 01:18:38 +0700

    login

--- src/components/profile/sideBar/sideBar.js
+++ src/components/profile/sideBar/sideBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 export const SideBar = () => {
     return (
         <div>
@@ -11,20 +12,26 @@ export const SideBar = () => {
                     </li>
                     <li>
                         <ul>
-                            <Link to="/profile/donationInformationProfile">Lịch sử hoạt động</Link>
+                            <Link to="/profile/donationInformationProfile">
+                                Lịch sử hoạt động
+                            </Link>
                             <li>
-                                <Link to="/profile/donationInformationProfile">Tài Trợ</Link>
+                                <Link to="/profile/donationInformationProfile">
+                                    Tài Trợ
+                                </Link>
                             </li>
                             <li>
-                                <Link to="/profile/receiveDonationProfile">Nhận tài trợ</Link>
+                                <Link to="/profile/receiveDonationProfile">
+                                    Nhận tài trợ
+                                </Link>
                             </li>
                         </ul>
                     </li>
                     <li>
-                        <Link to="/profile/changePasword">Mật khẩu</Link>
+                        <Link to="/profile/changepassword">Đổi Mật khẩu</Link>
                     </li>
                 </ul>
             </nav>
         </div>
     );
-}
+};
