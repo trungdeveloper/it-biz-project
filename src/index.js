8051bf98c15c5179181b9b6412e0808c9cecedc6
Nguyễn Tấn Trung <46466731+trungdeveloper@users.noreply.github.com>
2020-08-02 13:34:10 +0700

    Css 03 (#10)
    * CSS-03
    
    * css navbar
    
    * edit css undermenu
    
    * navbar done
    
    Co-authored-by: cuongphan <cuong.phan@student.passerellesnumeriques.org>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./css/bootstrap.css";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
