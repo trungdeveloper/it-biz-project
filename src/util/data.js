15bcbae75235ec72a911a7b8cbc4b0792b6ca487
cuong-phanminh <46159784+cuong-phanminh@users.noreply.github.com>
2020-09-15 22:50:22 +0700

    Tv 29 (#43)
    * edit data file and css for img
    
    * edit data file and css for img

--- src/util/data.js
+++ src/util/data.js
@@ -1,62 +1,62 @@
 export const donations = [
     {
         id: "a",
-        category_id: "1",
+        category_id: "0ozuvAiihxN2X4nlC3Zd",
         condition: "new",
         date: "12/09/2020",
         description: "Bếp Hồng Ngoại Sunhouse SHD6011 - Đen- hàng chính hãng",
         imgUrl:
             "https://firebasestorage.googleapis.com/v0/b/it-biz-52ebe.appspot.com/o/donation%2Fbep-hong-ngoai.jfif?alt=media&token=49b96a4e-6d57-4a52-aefb-67c6114ce619",
-        name: "Nguyễn Tấn Trung",
+        name: "Bếp Hồng Ngoại",
         status: "chờ xác nhận",
         uid: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
     },
     {
         id: "b",
-        category_id: "7",
+        category_id: "soN9xqfYXlRZXWy3MKzY",
         condition: "old",
         date: "12/09/2020",
         description:
             "Xe đạp không còn nhu cầu dùng nữa nên muốn chia sẽ cho những hoàn cảnh khó khăn có nhu cầu muốn dùng.",
         imgUrl:
             "https://firebasestorage.googleapis.com/v0/b/it-biz-52ebe.appspot.com/o/donation%2Fxe-dap.jfif?alt=media&token=4f30f252-cc78-426d-9d70-43f77dba8488",
-        name: "Nguyễn Tấn Trung",
+        name: "Xe đạp",
         status: "chờ xác nhận",
-        uid: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
+        uid: "mmsIWmol93fvJ7T4i6lGThf20xK2",
     },
     {
         id: "c",
-        category_id: "7",
+        category_id: "soN9xqfYXlRZXWy3MKzY",
         condition: "new",
         date: "12/09/2020",
         description: "Xe 3 bánh cho người khuyết tật.",
         imgUrl:
             "https://firebasestorage.googleapis.com/v0/b/it-biz-52ebe.appspot.com/o/donation%2Fxe-ba-banh-cho-nguoi-khuyet-tat.jfif?alt=media&token=724dca3d-4246-407f-b0b3-0cd34c0e1adc",
-        name: "Nguyễn Tấn Trung",
+        name: "Xe 3 bánh cho người khuyết tật",
         status: "chờ xác nhận",
         uid: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
     },
     {
         id: "d",
-        category_id: "7",
+        category_id: "soN9xqfYXlRZXWy3MKzY",
         condition: "new",
         date: "12/09/2020",
         description: "Bộ sách giáo khoa lớp 4 còn mới",
         imgUrl:
             "https://firebasestorage.googleapis.com/v0/b/it-biz-52ebe.appspot.com/o/donation%2Fsach-giao-khoa-lop-4.jfif?alt=media&token=7a11dd42-5cfe-4df6-ae0b-27731c78cc81",
-        name: "Nguyễn Tấn Trung",
+        name: "Bộ sách giáo khoa lớp 4",
         status: "chờ xác nhận",
-        uid: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
+        uid: "RmPBVIBeorScBX8wvGmuGU5iDB52",
     },
     {
         id: "e",
-        category_id: "2",
+        category_id: "CmrmGnwBpG7VjWQpiDxK  ",
         condition: "new",
         date: "12/09/2020",
         description: "Máy tính Dell Corei3 Ram 8G",
         imgUrl:
             "https://firebasestorage.googleapis.com/v0/b/it-biz-52ebe.appspot.com/o/donation%2Fmay-tinh-ban.jfif?alt=media&token=8502eec1-d80a-4658-83be-b41075da7afa",
-        name: "Nguyễn Tấn Trung",
+        name: "Máy tính bàn Dell",
         status: "chờ xác nhận",
         uid: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
     },
@@ -70,7 +70,7 @@ export const plights = [
             "Hiện tại em Lê Thành Tâm là học sinh lớp 7a1 Trường Trung học cơ sở Tây Sơn, gia đình nghèo khổ bố bệnh nặng không lao động được một mình mẹ là lao động chính nuôi cả gia đình. Em cần các nhà hảo tâm tài trợ cho em một chiếc xe đạp để đến trường mỗi ngày.",
         need: "Xe đạp đến trường",
         status: "Xác nhận",
-        uId: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
+        uid: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
     },
     {
         id: "bb",
@@ -79,7 +79,7 @@ export const plights = [
             "Ông Nguyễn Toàn - bị bại liệt, vợ con bỏ đi nơi khác (hiện trú tại chung cư A2 Nguyễn Đức Trung, quận Thanh Khê); ",
         need: "Xe ba bánh",
         status: "Xác nhận",
-        uId: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
+        uid: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
     },
     {
         id: "cc",
@@ -88,7 +88,7 @@ export const plights = [
             "Chồng đau yếu, vợ nuôi con bị bại liệt. Đó là hoàn cảnh éo le của chị Nguyễn Thị Xuyến. Đã hơn 5 năm nay, vợ chồng anh chị gắn liền với những tiếng rên rỉ trong đớn  đau của cậu con trai Dương Quốc Việt, 6 tuổi.",
         need: "Hỗ trợ tài chính",
         status: "Xác nhận",
-        uId: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
+        uid: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
     },
     {
         id: "dd",
@@ -97,7 +97,7 @@ export const plights = [
             "Mẹ bị tai nạn không qua khỏi, em Nguyễn Thị Sáng đã đồng ý hiến tạng của mẹ mình để kịp thời cứu sống nhiều người khác. Mẹ mất, 3 chị em Sáng rơi vào hoàn cảnh hết sức khó khăn, phải về nương tựa nhà cậu ở xã Cẩm Nhượng huyện Cẩm Xuyên (Hà Tĩnh).",
         need: "Hỗ trợ tài chính",
         status: "Xác nhận",
-        uId: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
+        uid: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
     },
     {
         id: "ee",
@@ -106,7 +106,7 @@ export const plights = [
             "Nhà chị Nguyễn Thị Lon nằm sâu trong vườn, thuộc Tổ 11, ấp Tân Hưng, xã Khánh Thạnh Tân. Vóc dáng nhỏ, gầy yếu, lưng còm đã khiến chị Lon già hơn nhiều so với tuổi của mình (57 tuổi). Kết hôn sớm, anh Thà (chồng chị) bệnh tim qua đời cách nay hơn 4 năm, một mình lo tiền nong cho 5 người con, 2 trai và 3 gái nên chị vất vả trăm bề. Hiện con trai út và 2 người con gái của chị đã có gia đình ở xa, cô út thì đi làm thuê ở TP. Hồ Chí Minh, còn lại người con trai thứ tư đang làm mướn cho một chủ ghe bơm cát sông.“Tôi sống với thằng con thứ tư. Nay nó 26 tuổi nhưng không bình thường, do bị tai nạn lúc 14 tuổi phải mổ não. Nó làm không đủ ăn... Tôi không yên tâm khi cho nó đi theo ghe cát, nhưng ngoài làm việc này nó không biết làm gì cả”, chị Lon bày tỏ. Hàng ngày, chị Lon đi xin lá dừa về chuốt lấy cọng bán với giá 6.000 đồng/kg. Công việc này giúp cho chị kiếm được khoảng 15 - 20 ngàn đồng/ngày. Những lúc chị đau khớp hoặc bị đứt tay thì xem như không có đồng nào cả. Số tiền kiếm được, chị để dành mua gạo. Tranh thủ thời gian rảnh, chị xuống mương mò ốc, xúc tép để làm thức ăn qua ngày. “Lo chạy từng bữa cơm thì việc có cái nhà mới để ở tôi không dám mơ” - chị Lon bày tỏ.",
         need: "Hỗ trợ tài chính",
         status: "Xác nhận",
-        uId: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
+        uid: "RN4MOySY3ZNhbkoM8BBH9pHU3Dj1",
     },
 ];
 
