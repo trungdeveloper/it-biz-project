06429580d72023c4470e66dc1d0842ed5b16b3ef
Hồ Văn Huy <50701095+hohuypn@users.noreply.github.com>
2020-09-20 13:30:47 +0700

    TV-30: update table in profile page (#42)
    * TV-30: update table in profile page
    
    * TV-30: fix footer
    
    * TV-30: footer css

--- src/components/profile/donation.js
+++ src/components/profile/donation.js
@@ -8,109 +8,103 @@ export const UserDonate = () => {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     return (
-        <div className="container">
-            <div className="row">
-                <div className="user_profile">
-                    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <div className="entry-title">
-                            <h5>Thông tin tài trợ</h5>
-                        </div>
-                        <table className="table table-bordered table-hover">
-                            <thead className="user-table">
-                                <tr>
-                                    <th>Tên vật phẩm</th>
-                                    <th>Mô tả</th>
-                                    <th>Hình ảnh</th>
-                                    <th>Trạng thái</th>
-                                    <th>Hoàn cảnh được cho</th>
-                                    <th>Hành động</th>
-                                </tr>
-                            </thead>
-                            <tbody className="user-table">
-                                <tr>
-                                    <td>Ấm siêu tốc</td>
-                                    <td>
-                                        Đang dư mấy cái ấm siêu tốc,
-                                        <br /> sử dụng được nhưng không có nhu
-                                        cầu, muốn tặng lại cho ai cần
-                                    </td>
-                                    <td>
-                                        <button
-                                            type="button"
-                                            onClick={handleShow}
-                                            className="btn btn-info"
-                                        >
-                                            Hình ảnh
-                                        </button>
-                                    </td>
-                                    <td>Đang chờ xét duyệt</td>
-                                    <td></td>
-                                    <td className="action">
-                                        <button
-                                            type="button"
-                                            className="btn btn-warning"
-                                        >
-                                            Sửa
-                                        </button>
-                                        <button
-                                            type="button"
-                                            className="btn btn-danger"
-                                        >
-                                            Xóa
-                                        </button>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Ấm siêu tốc</td>
-                                    <td>
-                                        Đang dư mấy cái ấm siêu tốc,
-                                        <br /> sử dụng được nhưng không có nhu
-                                        cầu, muốn tặng lại cho ai cần
-                                    </td>
-                                    <td>
-                                        <button
-                                            type="button"
-                                            onClick={handleShow}
-                                            className="btn btn-info"
-                                        >
-                                            Hình ảnh
-                                        </button>
-                                    </td>
-                                    <td>Đã trào tặng</td>
-                                    <td>Nguyễn Thị B</td>
-                                    <td className="action"></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <Modal
-                            show={show}
-                            onHide={handleClose}
-                            backdrop="static"
-                            aria-labelledby="contained-modal-title-vcenter"
-                            size="sm"
-                            centered
-                            keyboard={false}
-                        >
-                            <Modal.Header closeButton>
-                                <Modal.Title id="contained-modal-title-vcenter">
-                                    Hình ảnh
-                                </Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body className="modal-image">
-                                <img src={IMG} />
-                            </Modal.Body>
-                            <Modal.Footer>
-                                <Button
-                                    variant="secondary"
-                                    onClick={handleClose}
-                                >
-                                    Thoát
-                                </Button>
-                            </Modal.Footer>
-                        </Modal>
-                    </div>
+            <div className="user_profile">
+                <div className="entry-title">
+                    <h5>Thông tin tài trợ</h5>
                 </div>
+                <table className="table table-bordered table-hover">
+                    <thead className="user-table">
+                        <tr>
+                            <th>Tên vật phẩm</th>
+                            <th>Mô tả</th>
+                            <th>Hình ảnh</th>
+                            <th>Trạng thái</th>
+                            <th>Hoàn cảnh được cho</th>
+                            <th>Hành động</th>
+                        </tr>
+                    </thead>
+                    <tbody className="user-table">
+                        <tr>
+                            <td>Ấm siêu tốc</td>
+                            <td>
+                                Đang dư mấy cái ấm siêu tốc,
+                                <br /> sử dụng được nhưng không có nhu
+                                cầu, muốn tặng lại cho ai cần
+                            </td>
+                            <td>
+                                <button
+                                    type="button"
+                                    onClick={handleShow}
+                                    className="btn btn-info"
+                                >
+                                    Hình ảnh
+                                </button>
+                            </td>
+                            <td>Đang chờ xét duyệt</td>
+                            <td></td>
+                            <td className="action">
+                                <button
+                                    type="button"
+                                    className="btn btn-warning"
+                                >
+                                    Sửa
+                                </button>
+                                <button
+                                    type="button"
+                                    className="btn btn-danger"
+                                >
+                                    Xóa
+                                </button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Ấm siêu tốc</td>
+                            <td>
+                                Đang dư mấy cái ấm siêu tốc,
+                                <br /> sử dụng được nhưng không có nhu
+                                cầu, muốn tặng lại cho ai cần
+                            </td>
+                            <td>
+                                <button
+                                    type="button"
+                                    onClick={handleShow}
+                                    className="btn btn-info"
+                                >
+                                    Hình ảnh
+                                </button>
+                            </td>
+                            <td>Đã trào tặng</td>
+                            <td>Nguyễn Thị B</td>
+                            <td className="action"></td>
+                        </tr>
+                    </tbody>
+                </table>
+                    <Modal
+                        show={show}
+                        onHide={handleClose}
+                        backdrop="static"
+                        aria-labelledby="contained-modal-title-vcenter"
+                        size="sm"
+                        centered
+                        keyboard={false}
+                    >
+                        <Modal.Header closeButton>
+                            <Modal.Title id="contained-modal-title-vcenter">
+                                Hình ảnh
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body className="modal-image">
+                            <img src={IMG} />
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button
+                                variant="secondary"
+                                onClick={handleClose}
+                            >
+                                Thoát
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
             </div>
-        </div>
     );
 };
